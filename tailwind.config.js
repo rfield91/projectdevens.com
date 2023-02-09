cceccff898645b74a10c47f8d071d8d2fa4c7c0c
Mark Field <51247227+rfield91@users.noreply.github.com>
2023-02-08 20:50:37 -0500

    Added dynamically added color classes to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./app/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [
+        "bg-orange-200",
+        "bg-orange-500",
+        "bg-blue-200",
+        "bg-blue-500",
+        "bg-red-200",
+        "bg-red-500",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
